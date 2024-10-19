5c2b1fb5beb4dc7e8e5503dd4c4cac7a38df6af9
Nikolas Railan <davifpaes369@gmail.com>
2024-10-19 11:28:27 -0300

    mais mensagem

--- oimundo.java
+++ oimundo.java
@@ -4,5 +4,6 @@ public class oimundo{
     public static void main(String[] args) {
         System.out.println("OLÁ MUNDOOOOO!!!!!!");
         System.out.println("fala tu");
+        System.out.println("opa");
     }
 }
